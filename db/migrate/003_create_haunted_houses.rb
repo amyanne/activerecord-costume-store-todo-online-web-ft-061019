481d6fbbf3fdb12e8cde353016313b0b61c17420
Amy Westcott <amy.westcott@yahoo.com>
2019-08-05 16:34:54 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,7 +2,15 @@ class HauntedHouses < ActiveRecord::Migration[5.2]
   
   def change
     create_table :hauntedhouse do |t|
-      t.text :name 
+      t.string :name 
+      t.string :location 
+      t.string :theme 
+      t.integer :price 
+      t.boolean :family_friendly 
+      t.string :opening_date 
+      t.string :closing_date 
+      t.string :description 
+    end 
     
   end 
 end
